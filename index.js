8ff9523cccb74c4b426ed88797bb8130c2dba55b
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-03-17 11:59:13 +0300

    completed the lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";// changed the name variable t string susan
+const height = 39; // use a number that is less than 40 and greater than 0
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
